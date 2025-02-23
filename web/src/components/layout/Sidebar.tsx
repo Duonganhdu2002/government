eff63a94db6bad432ac2158aa6cdb601673bd8fb
Cangheuxinh <nhknga17032004@gmail.com>
2025-02-23 13:18:08 +0700

    +

--- web/src/components/layout/Sidebar.tsx
+++ web/src/components/layout/Sidebar.tsx
@@ -3,8 +3,8 @@
 import { useState } from "react";
 import { Sun, Plus } from "@medusajs/icons";
 import NavItem from "../common/NavItem";
-import Image from "next/image";
 import Link from "next/link";
+import { Avatar } from "@medusajs/ui";
 
 type SidebarProps = {
   sidebarOpen: boolean;
@@ -33,21 +33,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           md:static md:translate-x-0`}
       >
         <nav className="space-y-2 p-4 bg-white w-64">
-          <div className=" flex items-center gap-x-3 mb-3">
-            <Link href="https://www.example.com">
-              <div className="relative w-12 h-12 overflow-hidden rounded-full border border-gray-30 ">
-                {" "}
-                <Image
-                  src="https://images.pexels.com/photos/30375728/pexels-photo-30375728.jpeg"
-                  alt="Ảnh từ Pexels"
-                  width={64}
-                  height={64}
-                  className="object-cover"
+          <div className="flex items-center gap-x-3 mb-3">
+            
+              <div className="relative w-12 h-12 overflow-hidden rounded-full border border-gray-300 flex items-center justify-center">
+                <Avatar
+                  src="https://images.pexels.com/photos/29914956/pexels-photo-29914956.jpeg"
+                  fallback=""
+                  className="w-full h-full object-cover"
                 />
-                <div>Chú thích cho hình ảnh</div>
-              </div>{" "}
-            </Link>
-            <div className="text-lg font-medium flex items-center">Ten</div>
+              </div>
+              <div className="text-lg font-medium">Ten</div>
           </div>
           <NavItem
             href="/"
